9879ac67f39de165fc1dd5ca88b802b61d0bf8e5
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2022-03-26 09:50:59 +0600

    remove all items from selected cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,19 @@
 import React from "react";
 import './Cart.css';
 const Cart = (props) => {
+    const removeItems = () => {
+        document.getElementsByClassName('items')[0].innerHTML = '';
+    }
     return (
         <div className="cart">
             <h5 className="mb-3">Selected Books</h5>
-            <div>
+            <div className="items">
                 {
                     props.cart.map(product => <h5>{product.name}</h5>)
                 }
             </div>
             <button className="bg-primary text-white mb-3 px-3 py-2">CHOOSE 1 FORE ME</button><br />
-            <button className="bg-primary text-white mb-3 px-3 py-2">CHOOSE AGAIN</button>
+            <button onClick={() => removeItems()} className="bg-primary text-white mb-3 px-3 py-2">CHOOSE AGAIN</button>
         </div>
     )
 }
